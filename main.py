ba7e9b2909b560a48b2f456807f7c6b91e5c4293
gabrielviegas20 <gabriel.viegastrab@gmail.com>
2022-11-09 17:39:02 -0300

    commit3

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ df = pd.read_csv("C:/Users/gabri/OneDrive/Documentos/6Semestre/labprog/python/Tr
 df["Conceito Enade (Faixa)"] = pd.to_numeric(df["Conceito Enade (Faixa)"], errors="coerce")
 #ESCOLHENDO OM CURSO
 def Histrograma():
-    area = "ENGENHARIA MECÂNICA"
+    area = input("Escolha o curso: ")
     plot1 = df[df["Área de Avaliação"] == area]
     print(plot1[["Área de Avaliação", "Conceito Enade (Contínuo)", "Conceito Enade (Faixa)"]])
     plot1 = plot1.groupby("Sigla da IES").head(1)
@@ -18,10 +18,12 @@ def Histrograma():
     fig1 = px.histogram(plot1, x="Sigla da IES", y="Conceito Enade (Contínuo)")
     fig1.show()
     
-def StarPolar():
-    plot2 = df[df["Sigla da IES"]== "IME"]
+def StarPlots():
+    instituicao1 = input("Escolha a instituição 1 que deseja comparar(colocar sigla): ")
+    instituicao2 = input("Escolha a instituição 2 que deseja comparar(colocar sigla): ")
+    plot2 = df[df["Sigla da IES"]== instituicao1]
     print(plot2[["Sigla da IES","Área de Avaliação"]])
-    plot3 = df[df["Sigla da IES"]== "UFRJ"]
+    plot3 = df[df["Sigla da IES"]== instituicao2]
 
     fig2 = make_subplots(rows=1, cols=4, specs=[[{'type': 'polar'}] * 4] * 1)
     fig2.add_trace(
@@ -46,7 +48,8 @@ def StarPolar():
     fig2.show()
 
 def Medias():
-    fig= px.histogram(df, x="Conceito Enade (Contínuo)")
+    #fig= px.histogram(df, x="Conceito Enade (Contínuo)")
+    fig = go.Figure()
     print("A media do conceito geral eh: ")
     media = df["Conceito Enade (Contínuo)"].mean()
     print(round(media, 2))
@@ -59,14 +62,35 @@ def Medias():
     print("O desvio padrao do conceito geral eh: ")
     desvioPadrao = df["Conceito Enade (Contínuo)"].std()
     print(round(desvioPadrao, 2))
+    #######################
+    fig.add_trace(
+        go.Histogram(
+            x=df["Conceito Enade (Contínuo)"]
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=[media, media, media],
+            y=[0, 400],
+            name="media"
+        )
+    )
+
+
     fig.show()
     
     
     
-#print("Escolha que tipo de estatistica quer comparar: ")
-#print("1-Comparar qual faculdade tem o melhor conceito em um determinado curso: ")
-#print("2-Comparar qual faculdade tem o melhor conceito em um determinado curso: ")
-#print("3-Comparar qual faculdade tem o melhor conceito em um determinado curso: ")
-Histrograma()
-StarPolar()
-Medias()
+print("Escolha que tipo de estatistica quer comparar: ")
+print("1-Comparar qual faculdade tem o melhor conceito em um determinado curso: ")
+print("2-Comparar conceitos dos cursos entre 2 faculdades ")
+print("3-Calcular a media, variancia, mediana, desvio padrao dos conceitos de todas as instituições: ")
+escolha = int(input("Sua escolha: "))
+if escolha == 1:
+    Histrograma()
+else:
+    if escolha == 2:
+        StarPlots()
+    else:
+        Medias()
+
